1b9b38a468f60615e46a35c9e5ef2003f895aaac
Bom11010010 <bomudayo@gmail.com>
2026-08-18 23:23:54 +0000

    fix build.sh

--- opah/build.sh
+++ opah/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
 
+mkdir -p ../bin/
+
 export CMAKE_GENERATOR=Ninja
 
 cargo about generate about.hbs > ../third_party_licenses/THIRD-PARTY-LICENSES.html
@@ -8,6 +10,6 @@ cargo about generate about.hbs > ../third_party_licenses/THIRD-PARTY-LICENSES.ht
 cargo build --release
 
 rm -rf ../bin/opah
-cp -r ./target/release ../bin/opah_bin
 
-ln -s ../bin/opah_bin/opah ../bin/opah
+
+cp ./target/release/opah ../bin/opah
